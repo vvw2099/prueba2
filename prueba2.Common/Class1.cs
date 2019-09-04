7628dca1ef3aa30064458ea0ae28e442a7cbbb2a
vvw2099 <vvw2099@hotmail.com>
2019-09-04 15:39:46 -0600

    add common and add web

---
+++ prueba2.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace prueba2.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ prueba2.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prueba2.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
